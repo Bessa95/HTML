cbb04e17250973830fb0487aeda64ea1b8b28de5
Phelipe Bessa <phelipe.diogoti@gmail.com>
2022-12-02 00:39:50 -0300

    Create calcGorjeta

--- relogio/js/script.js
+++ relogio/js/script.js
@@ -2,5 +2,6 @@ function bonsModos(){
     var hora = new Date().getHours();
     var minuto = new Date().getMinutes();
     // console.log(hora);
-    document.write(hora+":"+minuto);
+    document.getElementById('relogio').innerHTML='hello';
+    // document.write(hora+":"+minuto);
 }
